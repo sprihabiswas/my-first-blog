34d4d805e468f8fa41ad5f771ce815c7175dbae0
Spriha Biswas <sprihabiswas@gmail.com>
2016-02-15 16:29:33 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_lists, name='post_lists'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
 
 def post_lists(request):
-	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts=Post.objects.all()
 	#Please note that we create a variable for our QuerySet: posts. 
 	#Treat this as the name of our QuerySet. From now on we can refer to it by this name.
 	return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	#Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post})
